581cdf5695e1209462cd98c86b151116b054597c
theozinner <tvz6123@gmail.com>
2021-03-09 22:22:51 -0600

    can now add and run proccess

--- project_3/aubatch.c
+++ project_3/aubatch.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <assert.h>
 
 #define BUFF_SIZE 15
 #define NUM_OF_CMD 5
@@ -24,7 +25,96 @@ u_int buf_head;
 u_int buf_tail;
 u_int count;
 
-char *cmd_buffer[BUFF_SIZE];
+
+void *commandLine();
+void *executor();
+int dispatch(char *cmd);
+
+void menu_execute(char *line, int isargs);
+int cmd_run(int nargs, char **args);
+int cmd_quit(int nargs, char **args);
+void showmenu(const char *name, const char *x[]);
+int cmd_helpmenu(int n, char **a);
+int cmd_dispatch(char *cmd);
+
+//job properties
+struct job_def {
+	char name[50];
+	int arg1, arg2, priority, burst, position;
+};
+
+//job buffer
+struct job_def jobBuffer[BUFF_SIZE - 1];
+
+/*
+ *  * The run command - submit a job.
+ *   */
+int cmd_run(int nargs, char **args) { 
+	if (nargs != 4) {
+		return EINVAL;
+	}
+	char cmd[nargs];
+	strcpy(cmd, args);
+	char *input = strtok(cmd, " ");
+	sscanf(input, "%s", &jobBuffer[buf_head].name);	
+	sscanf(input, "%d", &jobBuffer[buf_head].arg1);	
+	sscanf(input, "%d", &jobBuffer[buf_head].arg2);	
+	sscanf(input, "%d", &jobBuffer[buf_head].priority);	
+	sscanf(input, "%d", &jobBuffer[buf_head].burst);	
+	count = count + 1;
+	buf_head = buf_head + 1;
+	jobBuffer[buf_head].position = buf_head;
+	pthread_cond_signal(&cmd_buf_not_empty);
+	return 0; /* if succeed */
+}
+
+/*
+ *  * The quit command.
+ *   */
+int cmd_quit(int nargs, char **args) {
+	printf("Please display performance information before exiting AUbatch!\n");
+        exit(0);
+}
+
+/*
+ *  * Display menu information
+ *   */
+void showmenu(const char *name, const char *x[])
+{
+	int ct, half, i;
+        printf("\n");
+        printf("%s\n", name);
+        for (i=ct=0; x[i]; i++) {
+                ct++;
+	}
+	half = (ct+1)/2;
+        for (i=0; i<half; i++) {
+                printf("    %-36s", x[i]);
+                if (i+half < ct) {
+                        printf("%s", x[i+half]);
+                }
+		printf("\n");														       
+       	}
+        printf("\n");
+}
+
+static const char *helpmenu[] = {
+        "[run] <job> <time> <priority>       ",
+	"[quit] Exit AUbatch                 ",
+        "[help] Print help menu              ",
+        /* Please add more menu options below */
+        NULL
+};
+
+int cmd_helpmenu(int n, char **a)
+{
+        (void)n;
+        (void)a;
+        showmenu("AUbatch help menu", helpmenu);
+        return 0;
+}
+
+//char *cmd_buffer[BUFF_SIZE];
 
 int main() {
 	//initialize var
@@ -32,7 +122,7 @@ int main() {
 	buf_head = 0;
 	buf_tail = 0;
 		
-	printf("\m Welcome to Theo's bath job scheduler version 0.1\n enter help for a list of commands.");
+	printf("\n Welcome to Theo's bath job scheduler version 0.12\n enter help for a list of commands.");
 	pthread_t commandLineThread, executorThread;
 	
 	//concurrent threads
@@ -52,8 +142,8 @@ int main() {
 	pthread_cond_init(&cmd_buf_not_empty, NULL);
 
 	//join thread
-	pthread_join(commandLineThread);
-	pthread_join(dispatcherThread);
+	pthread_join(commandLineThread, NULL);
+	pthread_join(executorThread, NULL);
 	
 	//exit
 	exit(0);
@@ -93,8 +183,21 @@ int dispatch(char *cmd) {
 			printf("command line has too many words\n");
 			return E2BIG;
 		}
-		}	
-
+		args[nargs++] = word;
+	}
+		if (nargs==0) {
+			return 0;
+		}
+	for(i = 0; cmdtable[i].name; i++) {
+		if(*cmdtable[i].name && !strcmp(args[0], cmdtable[i].name)) {
+			assert(cmdtable[i].func != NULL);
+			//call function here maybe?
+			result = cmdtable[i].func(nargs, args);
+			return result;
+		}
+	}
+	printf("%s: Command not found\n", args[0]);
+	return EINVAL;
 }
 
 
@@ -103,7 +206,7 @@ int dispatch(char *cmd) {
 void *commandLine() {
 
 	u_int i;
-	char num_str[8]
+	char num_str[8];
 	size_t command_size[64];
 	char *input;
 	char inputA[100];
@@ -111,20 +214,22 @@ void *commandLine() {
 	for (i = 0; i < NUM_OF_CMD; i++) {
 		pthread_mutex_lock(&cmd_queue_lock);
 
-		while (count == CMD_BUF_SIZE) {
+		while (count == BUFF_SIZE) {
 			pthread_cond_wait(&cmd_buf_not_full, &cmd_queue_lock);
 		}
 		pthread_mutex_unlock(&cmd_queue_lock);
 		//prompt for input
 		printf("\n>");
 		getline(&input, &command_size, stdin);
-dispatch(input);
+		dispatch(input);
 	}
 
 }
 
 void *executor() {
 	u_int i;
+
+	char arg1[50], arg2[50];
 	for (i = 0; i < NUM_OF_CMD; i++) {
 		pthread_mutex_lock(&cmd_queue_lock);
 
@@ -133,14 +238,20 @@ void *executor() {
 	}
 	count--;
 	
-	system(cmd_buffer[buf_tail]); //change later to execv
-	free(cmd_buffer[buf_tail]); // change this too
-	
+	//system(jobBuffer[buf_tail]); //change later to execv
+	//free(jobBuffer[buf_tail]); // change this too
+	pid_t run = fork();
+
+	if(run == 0) {
+		execv("./process",(char*[]){"./process",arg1,arg2,NULL});
+	}
+
+	printf("its happening!");	
 	buf_tail++;
 	if (buf_tail == BUFF_SIZE) {
 		buf_tail = 0;
 	}
-	pthread_cond_signal(&cmd_buf_notfull);
+	pthread_cond_signal(&cmd_buf_not_full);
 	pthread_mutex_unlock(&cmd_queue_lock);
 	} //end for loop
 }
